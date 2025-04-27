8002ea2b836f4e4f4043a9ccc58d91317db775b5
bloodbornefanNo1 <ats20112007@gmail.com>
2025-04-27 16:32:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,181 @@
+let hand = 0
+let chipslist = []
+let rungame = true
+let main_d = document.querySelector('.main_d')
+let new_card = document.querySelector(".new_card")
+let count = document.querySelector(".chips")
+let enemy = document.querySelector(".enemypoints")
+let hands = document.querySelector(".hands")
+let e = 2
+let a = 0
+let f = 5
+let g = 1
+while (hand < e) {
+
+    a = Math.round((Math.random() * 34) + 1)
+    chipslist.push(a)
+    const img = document.createElement("img")
+    img.src = `./cards/${a}.webp`
+    main_d.appendChild(img)
+    hand++
+
+}
+
+function handler_new_card() {
+    f -= 1
+    hands.innerText = f
+    a = Math.round((Math.random() * 34) + 1)
+    chipslist.push(a)
+    const img = document.createElement("img")
+    img.src = `./cards/${a}.webp`
+    main_d.appendChild(img)
+    chipscount()
+    if (f < 1) {
+        new_card.removeEventListener("click", handler_new_card)
+    }
+}
+
+hands.innerText = f
+new_card.addEventListener('click',
+    handler_new_card);
+    let chips = 0
+function chipscount() {
+
+
+
+
+    let c = 0
+
+    const chipslen = chipslist.length
+    while (c < chipslen) {
+        let d = chipslist[c]
+        if (d < 3) {
+            chips += 2
+            c++
+        }
+        else if (5 > d & d >= 3) {
+            chips += 3
+            c++
+        }
+        else if (5 <= d & d < 8) {
+            chips += 4
+            c++
+        }
+        else if (8 <= d & d < 10) {
+            chips += 5
+            c++
+        }
+        else if (10 <= d & d < 11) {
+            chips += 6
+            c++
+        }
+        else if (11 <= d & d < 13) {
+            chips += 7
+            c++
+        }
+        else if (13 <= d & d < 14) {
+            chips += 8
+            c++
+        }
+        else if (14 <= d & d < 17) {
+            chips += 9
+            c++
+        }
+        else if (17 <= d & d < 21) {
+            chips += 10
+            c++
+        }
+        else if (21 <= d & d < 25) {
+            chips += 3
+            c++
+        }
+        else if (25 <= d & d < 28) {
+            chips += 4
+            c++
+        }
+        else if (28 <= d & d < 32) {
+            chips += 5
+            c++
+        }
+        else if (32 <= d & d <= 35) {
+            chips += 11
+            c++
+        }
+        count.innerText = `chips: ${chips}`
+
+    }
+}
+chipscount()
+h = 0
+let enemyi = 0
+let enemychipslist = []
+let k = 2
+while (h < k) {
+        enemyi = Math.round((Math.random() * 34) + 1)
+        enemychipslist.push(enemyi)
+        h++
+    }
+    let enemychips = 0
+
+    let enemyc = 0
+function enemycards() {
+
+    
+
+    const enemychipslen = enemychipslist.length
+    while (enemyc < enemychipslen) {
+        let enemyd = enemychipslist[enemyc]
+        enemyc++
+        if (enemyd < 3) {
+            enemychips += 2
+        }
+        else if (5 > enemyd & enemyd >= 3) {
+            enemychips += 3
+        }
+        else if (5 <= enemyd & enemyd < 8) {
+            enemychips += 4
+        }
+        else if (8 <= enemyd & enemyd < 10) {
+            enemychips += 5
+        }
+        else if (10 <= enemyd & enemyd < 11) {
+            enemychips += 6
+        }
+        else if (11 <= enemyd & enemyd < 13) {
+            enemychips += 7
+        }
+        else if (13 <= enemyd & enemyd < 14) {
+            enemychips += 8
+        }
+        else if (14 <= enemyd & enemyd < 17) {
+            enemychips += 9
+        }
+        else if (17 <= enemyd & enemyd < 21) {
+            enemychips += 10
+        }
+        else if (21 <= enemyd & enemyd < 25) {
+            enemychips += 3
+        }
+        else if (25 <= enemyd & enemyd < 28) {
+            enemychips += 4
+        }
+        else if (28 <= enemyd & enemyd < 32) {
+            enemychips += 5
+        }
+        else if (32 <= enemyd & enemyd <= 35) {
+            enemychips += 11
+        }
+        
+    }
+    if (enemychips < 15){
+            enemyi = Math.round((Math.random() * 34) + 1)
+        enemychipslist.push(enemyi)
+        h++
+        enemycards()
+        }
+    alert(enemychips)
+    
+}
+enemycards()
+console.log(enemychipslist)
+console.log(chipslist)
